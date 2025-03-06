26bd9ccf8c1e147029b9e459a223fab1ae9adefa
Богдан Пята <pjata21@gmail.com>
2025-03-06 23:56:58 +0200

    Добавьте файлы проекта.

---
+++ Deadfish/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    public class Deadfish
+    {
+        public static int[] Parse(string data)
+        {
+            int count = 0;
+
+            for (int i = 0; i < data.Length; i++) { 
+                
+
+            }
+
+            return new int[count];
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Parse("iiisdoso"));
+    }
+
+    
+}
